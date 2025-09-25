5ca3ec786f7eefd91812c4173ca1aaf59b1d53b9
Joakim Englund <joakimenglund@protonmail.com>
2025-09-25 12:41:45 +0200

    Messed up main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,6 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Person customer1 = new Customer("Kalle", LocalDate.of(1990, 1, 2));
-        Employee employee = new Employee("Pelle", LocalDate.of(1991, 3, 4), 25000);
-        Customer customer2 = new Customer("Lisa", LocalDate.of(1992, 5, 6));
-        Product product = new Product("Ris");
-
-        customer1.getName();
-        customer2.getName();
-        // customer1.getCheckoutCart(); // Cannot resolve method 'getCheckoutCart' in 'Person'
-        customer2.getCheckoutCart();
-
-
-        customer1.introducePerson();
-        customer2.introducePerson();
-        employee.introducePerson();
-
         System.out.println("Screw you! I'm gonna trash this place!");
     }
 }
